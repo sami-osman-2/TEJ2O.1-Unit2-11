9b18da0e9834f647fa1f41695c874d6a2b7d9469
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-11-27 01:00:10 -0500

    linted ts

--- main.ts
+++ main.ts
@@ -3,25 +3,25 @@
  * Created by: Sami Osman
  * Created on: Nov 2025
  * This program generates 2 random numbers and compares them on shake.
-*/
+ */
 
-let num1 = Math.randomRange(0, 99)
-let num2 = Math.randomRange(0, 99)
+let num1 = Math.randomRange(0, 99);
+let num2 = Math.randomRange(0, 99);
 
-basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Happy);
 
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(num1)
-})
+  basic.showNumber(num1);
+});
 
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(num2)
-})
+  basic.showNumber(num2);
+});
 
 input.onGesture(Gesture.Shake, function () {
-    if (num1 < num2) {
-        basic.showString("1<2")
-    } else {
-        basic.showString("1>2")
-    }
-})
+  if (num1 < num2) {
+    basic.showString("1<2");
+  } else {
+    basic.showString("1>2");
+  }
+});
