a663e0d860539e1d3b696da6bc61562c3b3c22fe
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-11-27 00:23:50 -0500

    finished typescript

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sami Osman
+ * Created on: Nov 2025
+ * This program generates 2 random numbers and compares them on shake.
 */
 
-basic.showString('Hello, World!')
+let num1 = Math.randomRange(0, 99)
+let num2 = Math.randomRange(0, 99)
+
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(num1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(num2)
+})
+
+input.onGesture(Gesture.Shake, function () {
+    if (num1 < num2) {
+        basic.showString("1<2")
+    } else {
+        basic.showString("1>2")
+    }
+})
